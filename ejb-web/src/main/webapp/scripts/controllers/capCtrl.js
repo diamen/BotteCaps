7773f2cb2390f8e3754986e40d11005b3eee9ce4
Michał Stobiński <mstopa23@gmail.com>
2016-07-04 22:15:01 +0200

    [WEB] Dodanie informacji o tym, czy kapsel jest piwny

--- ejb-web/src/main/webapp/scripts/controllers/capCtrl.js
+++ ejb-web/src/main/webapp/scripts/controllers/capCtrl.js
@@ -8,6 +8,8 @@ angular.module('bcControllers')
 			$scope.cap = data;
 			$scope.capsrc = base64Service.base64ToUrl(data.base64);
 			
+			$scope.beerLabel = data.beer === 1 ? 'Piwo' : 'Niepiwo';
+			
 			restService.photoController().getBrand(data.brand_id).success(function(d) {
 				$scope.brand = d;
 			});
